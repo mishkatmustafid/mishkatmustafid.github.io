382e400192480fb33a958c72ce58bbec1632e7d4
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-05-15 18:31:51 +0600

    intro section created

---
+++ src/components/intro.tsx
@@ -0,0 +1,18 @@
+export default function Intro() {
+  return (
+    <section id="home" className="p-5 text-center ">
+      <div className="container">
+        <span className="display-2 py-3">Hey, I am Mishkat Mustafid</span>
+        <p className="h2 text-secondary p-4 m-3">
+          A full-stack developer, specializing in building the frontend of
+          websites and web applications, with a keen focus on creating engaging
+          user experiences that contribute to the overall success of the
+          product.
+        </p>
+        <button className="rounded btn-warning py-1 px-3 display-6">
+          Projects
+        </button>
+      </div>
+    </section>
+  )
+}
