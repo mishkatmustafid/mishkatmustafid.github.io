fd85065030c3a98d9f812f2968a2ee61c16f9994
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-11-16 12:42:46 +0600

    website content as constants

---
+++ src/constants/index.js
@@ -0,0 +1,28 @@
+// import {} from "../assets"
+
+export const navLinks = [
+  {
+    id: "about",
+    title: "About",
+  },
+  {
+    id: "work",
+    title: "Work",
+  },
+  {
+    id: "contact",
+    title: "Contact",
+  },
+];
+
+const services = [];
+
+const technologies = [];
+
+const experiences = [];
+
+const testimonials = [];
+
+const projects = [];
+
+export { services, technologies, experiences, testimonials, projects };
