d249a2f1006757d74afcd3d7c4dab557aa24ad53
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-11-19 22:36:07 +0600

    experience section updated

--- src/constants/index.js
+++ src/constants/index.js
@@ -131,6 +131,34 @@ const technologies = [
     icon: docker,
   },
 ];
+const experiences = [
+  {
+    title: "Backend Developer",
+    company_name: "Tiger Trade",
+    icon: creator,
+    iconBg: "#E6DEDD",
+    date: "Feb 2022 - Sep 2022",
+    points: [
+      "Developed secure Python code in the FastAPI framework, implementing token timeouts for enhanced authentication security.",
+      "Engineered scrapers and crawlers for data retrieval from public domains, concurrently building APIs and data clients for efficient data consumption.",
+      "Conducted thorough troubleshooting, testing, and debugging, enhancing code quality and system efficiency.",
+      "Contributed to agile Scrum teams, addressing scalability issues, implementing robust security measures, and crafting reusable code for future projects.",
+    ],
+  },
+  {
+    title: "Full stack Developer",
+    company_name: "Bengal InfoTech",
+    icon: web,
+    iconBg: "#E6DEDD",
+    date: "Feb 2021 - Aug 2021",
+    points: [
+      "Led the entire implementation and deployment process, including code review, debugging, and issue resolution, ensuring the successful execution of software projects.",
+      "Provided comprehensive software application engineering and maintenance throughout the development lifecycle, including the creation of unit test cases for testing and automation.",
+      "Oversaw the implementation of automated build and deployment pipelines, managing development milestones from initiation to final delivery and actively engaging with customers to gather feedback and address concerns.",
+      "Designed and implemented scalable applications for data extraction and analysis, authored code fixes and enhancements, and tested/deployed highly available software products for inclusion in future releases and patches.",
+    ],
+  },
+];
 
 const testimonials = [];
 
