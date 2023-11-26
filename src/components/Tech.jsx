da37d8e66b4c14849b17a7bc40da4243374a4a0b
Mishkat Mustafid <mishkatmustafid@gmail.com>
2023-11-27 01:47:03 +0600

    tech section title added

--- src/components/Tech.jsx
+++ src/components/Tech.jsx
@@ -1,16 +1,26 @@
+import { motion } from "framer-motion";
+
 import { BallCanvas } from "./canvas";
+import { styles } from "../styles";
 import { SectionWrapper } from "../hoc";
 import { technologies } from "../constants";
+import { textVariant } from "../utils/motion";
 
 const Tech = () => {
   return (
-    <div className='flex flex-row flex-wrap justify-center gap-10'>
-      {technologies.map((technology) => (
-        <div className='w-28 h-28' key={technology.name}>
-          <BallCanvas icon={technology.icon} />
-        </div>
-      ))}
-    </div>
+    <>
+      <motion.div variants={textVariant()}>
+        <p className={styles.sectionSubText}>Tech I Use</p>
+        <h2 className={styles.sectionHeadText}>Tech Stack.</h2>
+      </motion.div>
+      <div className='flex flex-row flex-wrap justify-center gap-10'>
+        {technologies.map((technology) => (
+          <div className='w-28 h-28' key={technology.name}>
+            <BallCanvas icon={technology.icon} />
+          </div>
+        ))}
+      </div>
+    </>
   );
 };
 
